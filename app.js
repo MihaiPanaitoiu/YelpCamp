3346bc963ceb859ce3a8a4bee644514eead1be1a
Mihai Panaitoiu <varusovarus1328@gmail.com>
2021-09-25 14:24:27 +0300

    Added displaying of reviews

--- app.js
+++ app.js
@@ -106,7 +106,7 @@ app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) =
 
 //show route
 app.get('/campgrounds/:id', catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
+    const campground = await Campground.findById(req.params.id).populate('reviews');
     res.render('campgrounds/show', { campground })
 }));
 

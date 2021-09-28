af88cad7063c8ec9b468763b97ae09ddc55b5d01
Mihai Panaitoiu <varusovarus1328@gmail.com>
2021-09-28 18:24:48 +0300

    Added flash error partial

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ app.use(flash());
 
 app.use((req, res, next) => {
     res.locals.success = req.flash('success');
-    // res.locals.error = req.flash('error');
+    res.locals.error = req.flash('error');
     next();
 })
 
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -40,12 +40,20 @@ router.post('/', validateCampground, catchAsync(async (req, res, next) => {
 //show route
 router.get('/:id', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id).populate('reviews');
+    if(!campground) {
+        req.flash('error', 'Cannot find the campground');
+        res.redirect('/campgrounds');
+    }
     res.render('campgrounds/show', { campground })
 }));
 
 //route to get the edit form
 router.get('/:id/edit', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
+    if(!campground) {
+        req.flash('error', 'Cannot find the campground');
+        res.redirect('/campgrounds');
+    }
     res.render('campgrounds/edit', { campground })
 }));
 

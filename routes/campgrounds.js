673c54e8816ec2111d33a53c3483102962d4c68e
Mihai Panaitoiu <varusovarus1328@gmail.com>
2021-09-29 14:48:25 +0300

    Added more complex authorization to reviews model

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -30,7 +30,12 @@ router.post('/', isLoggedIn, catchAsync(async (req, res, next) => {
 
 //show route
 router.get('/:id', catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id).populate('reviews').populate('author');
+    const campground = await Campground.findById(req.params.id).populate({
+        path: 'reviews',
+        populate: {
+            path: 'author'
+        }
+    }).populate('author');
     if(!campground) {
         req.flash('error', 'Cannot find the campground');
         res.redirect('/campgrounds');
--- routes/reviews.js
+++ routes/reviews.js
@@ -3,15 +3,16 @@ const router  = express.Router({mergeParams: true});
 const catchAsync = require("../utils/catchAsync");
 const Campground = require("../models/campground");
 const Review = require("../models/review");
-const {validateReview} = require('../middleware');
+const {validateReview, isLoggedIn, isReviewAuthor} = require('../middleware');
 
 
 
 //POST review route
-router.post('/', validateReview, catchAsync(async (req, res) => {
-    const {id} = req.params
-    const campground = await Campground.findById(id)
-    const review = new Review(req.body.review)
+router.post('/', isLoggedIn, validateReview, catchAsync(async (req, res) => {
+    const {id} = req.params;
+    const campground = await Campground.findById(id);
+    const review = new Review(req.body.review);
+    review.author = req.user._id;
     campground.reviews.push(review);
     await review.save();
     await campground.save();
@@ -20,8 +21,8 @@ router.post('/', validateReview, catchAsync(async (req, res) => {
 }))
 
 //delete route for reviews. Delete the reviews and the object id from the reviews array
-router.delete('/:reviewId', catchAsync(async (req, res) => {
-    const {id, reviewId} = req.params
+router.delete('/:reviewId',isLoggedIn,isReviewAuthor, catchAsync(async (req, res) => {
+    const {id, reviewId} = req.params;
     await Campground.findByIdAndUpdate(id, {$pull: {reviews:reviewId}})
     await Review.findByIdAndDelete(reviewId);
     req.flash('success', 'Successfully deleted the review');

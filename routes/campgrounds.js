7db508609d2a052c88525898b75bf7729198737d
Mihai Panaitoiu <varusovarus1328@gmail.com>
2021-09-29 13:37:35 +0300

    Moved authorization logic to a middleware

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -2,25 +2,13 @@ const express = require('express');
 const catchAsync = require("../utils/catchAsync");
 const Campground = require("../models/campground");
 const router  = express.Router();
-//requiring our custom ExpressError
-const ExpressError = require('../utils/ExpressError')
-const {campgroundSchema} = require("../schemas.js");
-const {isLoggedIn} = require('../middleware');
+const {isLoggedIn, isAuthor, validateCampground} = require('../middleware');
 
 
-const validateCampground = (req, res, next) => {
-    //defining our campgroundSchema, that will validate the data before sending it to mongoose
-    const { error } = campgroundSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    } else {
-        next();
-    }
-}
 
 
 //basic index route
+
 router.get('/', catchAsync(async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', { campgrounds })
@@ -51,41 +39,27 @@ router.get('/:id', catchAsync(async (req, res) => {
 }));
 
 //route to get the edit form
-router.get('/:id/edit',isLoggedIn, catchAsync(async (req, res) => {
-    const { id } = req.params;
+router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(async (req, res) => {
+    const {id} = req.params;
     const campground = await Campground.findById(id);
     if(!campground) {
         req.flash('error', 'Cannot find the campground');
         res.redirect('/campgrounds');
     }
-    if (!campground.author.equals(req.user._id)) {
-        req.flash('error', 'You do not have permission to do that');
-        return res.redirect(`/campgrounds/${id}`);
-    }
     res.render('campgrounds/edit', { campground })
 }));
 
 //PUT request to send the data from the edit form
-router.put('/:id',isLoggedIn, validateCampground, catchAsync(async (req, res) => {
+router.put('/:id',isLoggedIn, isAuthor, validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
-    const campground = await Campground.findById(id);
-    if (!campground.author.equals(req.user._id)) {
-        req.flash('error', 'You do not have permission to do that');
-        return res.redirect(`/campgrounds/${id}`);
-    }
-    const camp = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     req.flash('success', 'Successfully updated campground');
-    res.redirect(`/campgrounds/${camp._id}`)
+    res.redirect(`/campgrounds/${campground._id}`)
 }));
 
 //DELETE REQUEST
-router.delete('/:id',isLoggedIn, catchAsync(async (req, res) => {
+router.delete('/:id',isLoggedIn, isAuthor, catchAsync(async (req, res) => {
     const { id } = req.params;
-    const campground = await Campground.findById(id);
-    if (!campground.author.equals(req.user._id)) {
-        req.flash('error', 'You do not have permission to do that');
-        return res.redirect(`/campgrounds/${id}`);
-    }
     await Campground.findByIdAndDelete(id);
     req.flash('success', 'Successfully deleted the campground');
     res.redirect('/campgrounds')
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,21 +1,10 @@
 const express = require('express');
 const router  = express.Router({mergeParams: true});
 const catchAsync = require("../utils/catchAsync");
-const ExpressError = require('../utils/ExpressError');
 const Campground = require("../models/campground");
 const Review = require("../models/review");
-const {reviewSchema} = require("../schemas.js");
+const {validateReview} = require('../middleware');
 
-
-const validateReview = (req, res, next) => {
-    const {error} = reviewSchema.validate(req.body);
-    if(error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    } else {
-        next();
-    }
-}
 
 
 //POST review route
